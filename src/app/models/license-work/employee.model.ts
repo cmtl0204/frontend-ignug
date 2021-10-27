14ac11f4f5c315ae5e9807e2a8ff9769a390e002
LuisGerardo17 <you@example.com>
2021-10-27 16:03:56 -0500

    create reason.model.ts, employee.model.ts

---
+++ src/app/models/license-work/employee.model.ts
@@ -0,0 +1,5 @@
+
+export interface EmployeeModel {
+    id?: number;
+    userId?:number;
+  }
---
+++ src/app/models/license-work/reason.model.ts
@@ -0,0 +1,10 @@
+
+export interface ReasonModel {
+    id?: number;
+    name?: string;
+    descriptionOne?: string;
+    descriptionTwo?: string;
+    descountableHolidays?: boolean;
+    daysMin?: number;
+    daysMax?:number;
+  }
