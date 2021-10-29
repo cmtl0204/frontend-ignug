6cd3fefc0ad0ec3afe673de105d13f402fad7600
ErickDC <erickdc.lol@gmail.com>
2021-10-29 11:05:12 -0500

    update component

--- src/app/pages/license-work/license-work.component.ts
+++ src/app/pages/license-work/license-work.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-example',
+  selector: 'app-license-work',
   templateUrl: './license-work.component.html',
   styleUrls: ['./license-work.component.scss']
 })
